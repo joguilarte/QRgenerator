497cc56c9d4212277514a8a99ffc80ff1ed7dc79
joguilarte <33516868+joguilarte@users.noreply.github.com>
2023-10-23 17:42:02 -0300

    Update GeneradorQR.ps1

--- GeneradorQR.ps1
+++ GeneradorQR.ps1
@@ -1,4 +1,4 @@
-﻿$url = 'https://forms.gle/kCStHfKrSMBXPvyE7'  # Reemplaza esto con tu URL
+﻿$url = 'URL'  # Reemplaza esto con tu URL
 $qrUrl = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' + $url
 
 Invoke-WebRequest -Uri $qrUrl -OutFile 'RUTA DE SALIDA'
